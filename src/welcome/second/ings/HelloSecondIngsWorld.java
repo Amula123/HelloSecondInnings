69b6328e491e98cb7c7e2e54385ea56fa95549b5
Amula123 <shamsunder2000@gmail.com>
2018-07-02 09:32:32 -0700

    Demo commit for Second Innings

---
+++ src/welcome/second/ings/HelloSecondIngsWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package welcome.second.ings;
+
+/**
+ * @author LENOVO
+ *
+ */
+public class HelloSecondIngsWorld {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Second Innings");
+
+	}
+
+}
---
+++ src/welcome/second/ings/Test.java
@@ -0,0 +1,19 @@
+package welcome.second.ings;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		
+		System.out.println("Hello Test");
+		
+		
+		//StringBuffer strarry = new StringBuffer("a sham sunder");
+		StringBuffer strarry = new StringBuffer("a sham sunder");
+		//System.out.println(strarry.lastIndexOf("a sham sunder"));
+		
+		
+		
+	}
+ }
